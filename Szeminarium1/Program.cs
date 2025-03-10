95e8c30b2a11ad38a02f35195a1f88516e2585fc
Boogie <148373295+Boogie231@users.noreply.github.com>
2025-03-10 21:49:56 +0200

    Title experiment
    vertexArray elrontva: törölve egy vessző, nem adott hibát de eltűnt a háromszög egyik része teljesen

--- Szeminarium1/Program.cs
+++ Szeminarium1/Program.cs
@@ -41,8 +41,9 @@ namespace Szeminarium1
         static void Main(string[] args)
         {
             WindowOptions windowOptions = WindowOptions.Default;
-            windowOptions.Title = "1. szeminárium - háromszög";
+            windowOptions.Title = "1. szeminárium - háromszög- TESZT";
             windowOptions.Size = new Silk.NET.Maths.Vector2D<int>(500, 500);
+            
 
             graphicWindow = Window.Create(windowOptions);
 
@@ -108,7 +109,7 @@ namespace Szeminarium1
             Gl.BindVertexArray(vao);
 
             float[] vertexArray = new float[] {
-                -0.5f, -0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f
                 +0.5f, -0.5f, 0.0f,
                  0.0f, +0.5f, 0.0f,
                  1f, 1f, 0f
